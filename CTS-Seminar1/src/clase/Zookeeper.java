bd2f351f969c5b0526db82f8690d927c621c747e
Adina Chivaran <chivaranadina18@stud.ase.ro>
2021-02-27 14:21:59 +0200

    adaugare clase Animal si Zookeeper

--- CTS-Seminar1/src/clase/Zookeeper.java
+++ CTS-Seminar1/src/clase/Zookeeper.java
@@ -1,5 +1,14 @@
 package clase;
 
 public class Zookeeper {
+   private String nume;
 
+public Zookeeper(String nume) {
+	super();
+	this.nume = nume;
+}
+   
+   public void feed(Animal animal) {
+	   System.out.println("Ingrijitorul "+this.nume+" hraneste animalul "+animal.getNume());
+   }
 }
